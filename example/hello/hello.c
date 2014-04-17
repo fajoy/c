d049548c78a445f0effc0ee26d33b18f8fbfb376
fajoy <wuminfajoy@gmail.com>
2014-04-18 00:31:13 +0800

    update.

--- example/hello/hello.c
+++ example/hello/hello.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-//ref http://www.cprogramming.com/tutorial/c/lesson1.html
+//ref http://www.gnu.org/software/libc/manual/html_node/index.html#toc_Top
 int
-main()
+main (int argc, char *argv[], char *envp[])
 {
-    printf("Hello World");
+    printf("Hello World\n");
 	return 0;
 }
 
