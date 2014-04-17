550b9e541945f288c6f6fe2dde803808535fd4c3
fajoy <wuminfajoy@gmail.com>
2014-04-17 23:53:55 +0800

    add readline

---
+++ example/readline/readline.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//ref http://www.gnu.org/software/libc/manual/html_node/Line-Input.html
+//ref http://www.gnu.org/software/octave/doc/interpreter/Formatted-Output.html
+int
+main()
+{
+    char *buf;
+    size_t n = 0;
+	int r = 0;
+    r = getline(&buf, &n, stdin);
+	printf("(%d)(%p)(%u)(%lu)%s",r,&buf,(unsigned int)n,strlen(buf),buf);
+	//debug use
+	//cat /dev/zero |./readline.o
+	return 0;
+}
+
